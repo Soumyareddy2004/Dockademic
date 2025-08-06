3ce4bafe39e9fcfa402e07243a7c802ac28e7772
Soumyareddy2004 <sowmyareddy1911@gmail.com>
2025-08-06 19:15:30 +0530

    Dockerized Flask MySQL App

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # ✅ Define database config properly
 db_config = {
     'host': os.environ.get("DB_HOST", "localhost"),
+    'port': int(os.environ.get("DB_PORT", 3306)),
     'user': os.environ.get("DB_USER", "root"),
     'password': os.environ.get("DB_PASSWORD", ""),
     'database': os.environ.get("DB_NAME", "school")
